3b1fcc2a9984b31fe1e8d534f2aafd6aff94d88f
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-05-03 17:13:19 +0200

    feat: add router & helper function for axios request

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    path: '/search/:tag',
+    name: 'searchResults',
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "searchResults" */ '../views/SearchResults.vue'),
+    props: true,
   },
 ];
 
